87ce0a2f16c477e209a19a5d0458e4bf9518933e
Manish Pandey <manish.pandey2@arm.com>
2023-06-16 16:53:16 +0100

    feat(ras): test to verify SErros synchronized at EL3 boundry
    Tests to verify SErrors which gets triggered as part of error
    synchronization at EL3 entry.
    In FFH mode, EL3 does nested exception handling while in KFH it
    reflectes it back to TF-A Tests.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I5a980ed49ee47ea2f0d5685ba8378d48393ad157

---
+++ tftf/tests/tests-ras-ffh-nested.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2023, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES	+=	$(addprefix tftf/tests/misc_tests/,		\
+	inject_ras_error.S 						\
+	test_ras_ffh_nested.c						\
+)

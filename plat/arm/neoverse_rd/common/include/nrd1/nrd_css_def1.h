61b321e9f8f3a3cd8398b65e5f733abc5577d1a4
Jerry Wang <Jerry.Wang4@arm.com>
2024-07-05 10:59:33 +0100

    refactor(neoverse_rd): refactor header files for first gen platforms
    TF-A underwent a major rehaul in directory structure. This change
    deals with refactoring the header files for first generation platforms
    grouped as nrd1 in TFTF to a structure that aligns with that of TF-A.
    
    Change-Id: I0931f369ff390ae000f49c021a8c9dfc17f34b97
    Signed-off-by: Jerry Wang <Jerry.Wang4@arm.com>

---
+++ plat/arm/neoverse_rd/common/include/nrd1/nrd_css_def1.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is limited to include the CSS specific memory and interrupt map
+ * definitions for the first generation platforms based on the A75, N1 and V1
+ * CPUs. There are minor differences in the memory map of these platforms and
+ * those differences are not in the scope of this file.
+ */
+
+#ifndef NRD_CSS_DEF1_H
+#define NRD_CSS_DEF1_H
+
+/*******************************************************************************
+ * CSS memory map related defines
+ ******************************************************************************/
+
+/* Sub-system Peripherals */
+#define NRD_DEVICE0_BASE		UL(0x2A000000)
+#define NRD_DEVICE0_SIZE		UL(0x26000000)
+
+/* Peripherals and PCIe expansion area */
+#define NRD_DEVICE1_BASE		UL(0x60000000)
+#define NRD_DEVICE1_SIZE		UL(0x20000000)
+
+/* DRAM base address and size */
+#define PLAT_ARM_DRAM1_BASE		UL(0x80000000)
+#define PLAT_ARM_DRAM1_SIZE		UL(0x80000000)
+
+/* AP Non-Secure UART related constants */
+#define NRD_CSS_NSEC_UART_BASE		UL(0x2A400000)
+
+#endif /* NRD_CSS_DEF1_H */
---
+++ plat/arm/neoverse_rd/common/include/nrd1/nrd_css_fw_def1.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is limited to include the CSS firmware specific definitions for
+ * the first generation platforms based on the A75, N1 and V1 CPUs.
+ */
+
+#ifndef NRD1_CSS_FW_DEF1_H
+#define NRD1_CSS_FW_DEF1_H
+
+#include "nrd_css_def1.h"
+
+/*******************************************************************************
+ * Console config
+ ******************************************************************************/
+
+#define NRD_CSS_NSEC_CLK_IN_HZ		UL(7372800)
+
+#endif /* NRD_CSS_FW_DEF1_H */

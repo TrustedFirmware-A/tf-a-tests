0145ec3459750f35a6546b3f015906356b7dc6cd
Manish Pandey <manish.pandey2@arm.com>
2024-08-12 17:59:54 +0100

    feat: skeleton for asymmetric feature testing capability
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I6da831043c9485de00ba29a22ebaea6e9cdb5f57

---
+++ tftf/tests/misc_tests/test_asymmetric_features.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch.h>
+#include <arch_helpers.h>
+#include <arm_arch_svc.h>
+#include <assert.h>
+#include <debug.h>
+#include <smccc.h>
+#include <sync.h>
+#include <tftf_lib.h>
+#include <platform_def.h>
+
+test_result_t test_asymmetric_features(void)
+{
+	return TEST_RESULT_SUCCESS;
+}

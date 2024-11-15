d11773152d7d6e2b4e6ca979568cabd6079e108b
Jens Wiklander <jens.wiklander@linaro.org>
2024-11-15 09:17:19 +0100

    feat(drivers): add arm generic timer interface
    Add an arm generic timer interface suitable to be used as callback from
    plat_timer_t.
    
    Change-Id: I883b990321db7a3c0cc4928c1dd15ff7a1936761
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- drivers/arm/timer/private_timer.c
+++ drivers/arm/timer/private_timer.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018, Arm Limited. All rights reserved.
+ * Copyright (c) 2024, 2026, Linaro Limited.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -71,3 +72,84 @@ void private_timer_restore(void)
 	write_cnthp_cval_el2(pcpu_timer_context[linear_id].cval);
 	write_cnthp_ctl_el2(pcpu_timer_context[linear_id].ctl);
 }
+
+/*
+ * Program arm generic timer to fire and interrupt after time_out_ms
+ * milliseconds.
+ */
+int arm_gen_timer_program(unsigned long time_out_ms)
+{
+	unsigned int cntp_ctl;
+	unsigned long long count_val;
+	unsigned int freq;
+
+	count_val = read_cntpct_el0();
+	freq = read_cntfrq_el0();
+	count_val += (freq * time_out_ms) / 1000;
+
+	/*
+	 * Ensure that we have programmed a timer interrupt for a time in
+	 * future. Else we will have to wait for the gentimer to rollover
+	 * for the interrupt to fire (which is 64 years).
+	 */
+	if (count_val < read_cntpct_el0())
+		return -1;
+
+	write_cntp_cval_el0(count_val);
+
+	/* Enable the timer */
+	cntp_ctl = read_cntp_ctl_el0();
+	set_cntp_ctl_enable(cntp_ctl);
+	clr_cntp_ctl_imask(cntp_ctl);
+	write_cntp_ctl_el0(cntp_ctl);
+
+	VERBOSE("%s : interrupt requested at sys_counter: %llu "
+		"time_out_ms: %ld\n", __func__, count_val, time_out_ms);
+
+	return 0;
+}
+
+static void disable_gentimer(void)
+{
+	uint32_t val;
+
+	/* Deassert and disable the timer interrupt */
+	val = 0;
+	set_cntp_ctl_imask(val);
+	write_cntp_ctl_el0(val);
+}
+
+/*
+ * Cancel the currently programmed arm generic timer interrupt
+ */
+int arm_gen_timer_cancel(void)
+{
+	disable_gentimer();
+	return 0;
+}
+
+/*
+ * Handler to acknowledge and de-activate the arm generic timer interrupt
+ */
+int arm_gen_timer_handler(void)
+{
+	disable_gentimer();
+	return 0;
+}
+
+/*
+ * Initializes the arm generic timer so that it can be used for programming
+ * timer interrupt.
+ */
+int arm_gen_timer_init(void)
+{
+	/* Disable the timer as the reset value is unknown */
+	disable_gentimer();
+
+	/* Initialise CVAL to zero */
+	write_cntp_cval_el0(0);
+
+	return 0;
+}
+
+

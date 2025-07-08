d8e55e736799329d42caa2ec1453fd479ee1388f
Yeoreum Yun <yeoreum.yun@arm.com>
2025-07-08 12:19:06 +0100

    feat(fvp): change ARM_SECURE_SERVICE_BUFFER_BASE
    As NS buffer is moved to NS_DRAM1 area [0],
    Change ARM_SECURE_SERVICE_BUFFER_BASE.
    
    Link: https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/40336 [0]
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Change-Id: Ic348305dcbcfa506e973952676b53136755aed5f

--- plat/arm/fvp/include/platform_def.h
+++ plat/arm/fvp/include/platform_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021, Arm Limited. All rights reserved.
+ * Copyright (c) 2018-2025, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -311,7 +311,7 @@
  * Note: This address has to match the one used in TF (see ARM_SP_IMAGE_NS_BUF_*
  * macros).
  ******************************************************************************/
-#define ARM_SECURE_SERVICE_BUFFER_BASE	0xff600000ull
+#define ARM_SECURE_SERVICE_BUFFER_BASE	0xfef00000ull
 #define ARM_SECURE_SERVICE_BUFFER_SIZE	0x10000ull
 
 #endif /* __PLATFORM_DEF_H__ */

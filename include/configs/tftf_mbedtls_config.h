caca5cc7406eea453bcd93df1639ba2407ae80ef
Lauren Wehrmeister <lauren.wehrmeister@arm.com>
2025-07-10 14:28:24 -0500

    feat(mbedtls): update mbedtls to version 3.6.4
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: I3ed56c27d7c67f5386d9c9a69b45b90a4a5f4b60

--- include/configs/tftf_mbedtls_config.h
+++ include/configs/tftf_mbedtls_config.h
@@ -21,8 +21,8 @@
 /* For snprintf function declaration */
 #include <stdio.h>
 
-/* This file is compatible with release 3.6.3 */
-#define MBEDTLS_CONFIG_VERSION         0x03060300
+/* This file is compatible with release 3.6.4 */
+#define MBEDTLS_CONFIG_VERSION         0x03060400
 
 /* Configuration file to build mbed TLS with the required features for TFTF */
 #define MBEDTLS_PLATFORM_MEMORY

1bf7a81590ac18b91fd7f989a274c1a11fb953a9
Michael Safwat <michael.safwat@arm.com>
2025-08-26 11:20:01 +0000

    plat: corstone1000: Add Cortex-A320 support
    Switch platform to GICv3 (GIC-600) for Corstone-1000 with Cortex-A320
    depending on CORSTONE1000_CORTEX_A320:
        - Define GICD and GICR bases.
        - Update the platform sources to include the GIC-V3 files.
    
    Move the NVM offset to prevent overlap with the TFTF firmware,
    which starts at 0x80000000 (TFTF_BASE).
    
    Introduce a new skip file tests_to_skip_cortex_a320 to be used when building
    TF-A-Tests with CORSTONE1000_CORTEX_A320=1. This ensures that tests which
    are not supported or cause traps on Corstone-1000 with Cortex-A320 are
    consistently skipped during execution.
    
    Skipped entries:
        CPU extensions/AMUv1 suspend/resume
        CPU extensions/Use trace buffer control Registers
    
    Change-Id: I0745f12b84b3a7cd7577f8eaec0a1e8d5cb65875
    Signed-off-by: Michael Safwat <michael.safwat@arm.com>
    Signed-off-by: Harsimran Singh Tungal <harsimransingh.tungal@arm.com>

--- plat/arm/corstone1000/platform.mk
+++ plat/arm/corstone1000/platform.mk
@@ -21,6 +21,16 @@ $(eval $(call assert_boolean,PLAT_SUPPORTS_NS_RESET))
 $(eval $(call add_define,TFTF_DEFINES,PLAT_SUPPORTS_NS_RESET))
 
 FIRMWARE_UPDATE := 0
+
+CORSTONE1000_CORTEX_A320   :=      0
+ifeq ($(CORSTONE1000_CORTEX_A320),1)
+$(eval $(call add_define,TFTF_DEFINES,CORSTONE1000_CORTEX_A320))
+endif
+
+ifeq (${CORSTONE1000_CORTEX_A320},1)
+PLAT_TESTS_SKIP_LIST    :=      plat/arm/corstone1000/tests_to_skip_cortex_a320.txt
+else
 PLAT_TESTS_SKIP_LIST    :=      plat/arm/corstone1000/tests_to_skip.txt
+endif
 
 include plat/arm/common/arm_common.mk

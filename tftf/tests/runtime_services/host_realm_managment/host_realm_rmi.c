7229d0781ea7e1870202d4aec2f7cc38db7e9604
AlexeiFedorov <Alexei.Fedorov@arm.com>
2026-02-19 13:31:22 +0000

    feat(realm): add PSMMU related RMI calls
    Add PSMMU RMI calls for DMA SMMUv3 Realm test.
    
    Change-Id: I04cef5939a07ff0fcba8c6116e41f0a4c45501b7
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

--- tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
+++ tftf/tests/runtime_services/host_realm_managment/host_realm_rmi.c
@@ -2139,3 +2139,27 @@ u_register_t host_rmi_vdev_validate_mapping(u_register_t rd,
 	*out_top = rets.ret1;
 	return rets.ret0;
 }
+
+u_register_t host_rmi_psmmu_activate(u_register_t psmmu_ptr, u_register_t params_ptr)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PSMMU_ACTIVATE, psmmu_ptr,
+						params_ptr}, 3U).ret0;
+}
+
+u_register_t host_rmi_psmmu_deactivate(u_register_t psmmu_ptr)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PSMMU_DEACTIVATE, psmmu_ptr},
+						2U).ret0;
+}
+
+u_register_t host_rmi_psmmu_st_l2_create(u_register_t psmmu_ptr, u_register_t sid)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PSMMU_ST_L2_CREATE, psmmu_ptr,
+						sid}, 3U).ret0;
+}
+
+u_register_t host_rmi_psmmu_st_l2_destroy(u_register_t psmmu_ptr, u_register_t sid)
+{
+	return host_rmi_handler(&(smc_args) {SMC_RMI_PSMMU_ST_L2_DESTROY, psmmu_ptr,
+						sid}, 3U).ret0;
+}

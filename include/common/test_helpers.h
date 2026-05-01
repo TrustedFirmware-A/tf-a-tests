31849e637613671fb9e1ccd53cc2a99bc32bc218
Boyan Karatotev <boyan.karatotev@arm.com>
2026-05-01 16:24:20 +0100

    feat(cpufeat): add simple tests for FEAT_SPE_EXC and FEAT_SPE_nVM
    These are simple trap tests with associated boilerplate and SMCCC
    FEATURE_AVAILABILITY defines.
    
    Change-Id: Iaa587bad6bf35aacef61e3fa580ce90ed7b4680c
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>

--- include/common/test_helpers.h
+++ include/common/test_helpers.h
@@ -547,6 +547,30 @@ typedef test_result_t (*test_function_arg_t)(void *arg);
 		}								\
 	} while (0)
 
+#define SKIP_TEST_IF_SPE_NOT_SUPPORTED()					\
+	do {									\
+		if (!is_feat_spe_supported()) {					\
+			tftf_testcase_printf("SPE not supported\n");		\
+			return TEST_RESULT_SKIPPED;				\
+		}								\
+	} while (0)
+
+#define SKIP_TEST_IF_SPE_EXC_NOT_SUPPORTED()					\
+	do {									\
+		if (!is_feat_spe_exc_supported()) {				\
+			tftf_testcase_printf("SPE_EXC not supported\n");	\
+			return TEST_RESULT_SKIPPED;				\
+		}								\
+	} while (0)
+
+#define SKIP_TEST_IF_SPE_NVM_NOT_SUPPORTED()					\
+	do {									\
+		if (!is_feat_spe_nvm_supported()) {				\
+			tftf_testcase_printf("SPE_nVM not supported\n");	\
+			return TEST_RESULT_SKIPPED;				\
+		}								\
+	} while (0)
+
 /* Helper macro to verify if system suspend API is supported */
 #define is_psci_sys_susp_supported()	\
 		(tftf_get_psci_feature_info(SMC_PSCI_SYSTEM_SUSPEND)		\
